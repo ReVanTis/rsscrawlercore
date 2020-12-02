043622f6980a7b3acd69b600b7f2dc51f0a9c366
ReVanTis <revantis0@gmail.com>
2020-12-02 16:32:30 +0800

    Add support for World of Tanks Shop item updates for CN 360 server.

--- Controllers/RssController.cs
+++ Controllers/RssController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Xml;
 using HtmlAgilityPack;
@@ -32,6 +34,14 @@ namespace rsscrawlercore.Controllers
         public DateTime PubDate { get; set; }
         public string Content { get; set; }
     }
+    public class WotShopEntry
+    {
+        public string WGId {get;set;}
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public DateTime PubDate { get; set; }
+        public string Content { get; set; }
+    }
     public class RssController : Controller
     {
         public static string DateTimeFormat = @"yyyy-MM-dd HH:mm:ss";
@@ -50,6 +60,19 @@ namespace rsscrawlercore.Controllers
                 return htmldocObject;
             }
         }
+        public string DownloadGzipString(string siteUrl)
+        {
+            using (var wc = new GZipWebClient())
+            {
+                wc.Headers[HttpRequestHeader.AcceptEncoding] = "gzip";
+                wc.Headers[HttpRequestHeader.UserAgent] = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0";
+                wc.Headers[HttpRequestHeader.Referer] = @"https://shop.wot.360.cn/main";
+                wc.Encoding = Encoding.UTF8;
+                string payload = wc.DownloadString(siteUrl);
+                return payload;
+            }
+        }
+        #region Gamersky feed
         private async Task<string> FormatGamerskyFeed(IEnumerable<GamerskyEntry> entries)
         {
             var sw = new StringWriterWithEncoding(Encoding.UTF8);
@@ -177,5 +200,99 @@ namespace rsscrawlercore.Controllers
 
             Console.WriteLine($"{DateTime.Now.ToString(DateTimeFormat)}:Cache Update Done.");
         }
+        #endregion
+
+        #region WoT Shop feed
+        [HttpGet, Route("/rss/wotshop360")]
+        public async Task<IActionResult> GetWoTShop360Async()
+        {
+            string WoTShopURL = @"https://shop.wot.360.cn/api/product/list?game_id=1&type=main";
+            string RawJson = DownloadGzipString(WoTShopURL);
+            JsonDocument shopResponse = JsonDocument.Parse(RawJson);
+            if(shopResponse.RootElement.GetProperty("errno").GetInt32() == 0)
+            {
+                List<WotShopEntry> Entries = new List<WotShopEntry>();
+                Console.WriteLine("Response no error, continue parsing...");
+                var list = shopResponse.RootElement.GetProperty("data").GetProperty("list").EnumerateArray().ToList();
+                foreach( var item in list)
+                {
+                    try
+                    {
+                        WotShopEntry e = new WotShopEntry();
+                        e.Title = item.GetProperty("name").GetString();
+                        e.Link = @"https://shop.wot.360.cn/detail.html?goods_id="+ item.GetProperty("product_code").GetString();
+                        var price = item.GetProperty("price").GetString();
+                        var picURL= item.GetProperty("pic").GetString();
+                        e.PubDate = DateTime.Parse(item.GetProperty("cache_time").ToString());
+                        e.Content = $"<img src={picURL}><br><p>{price} RMB</p><br>" + item.GetProperty("descript").GetString() + "<br><p>";
+                        foreach ( var i in item.GetProperty("package_content").EnumerateArray().ToList() )
+                        {
+                            e.Content = e.Content + "<br>" + i.GetProperty("content").GetString();
+                        }
+                        e.Content+="</p>";
+                        Entries.Add(e);
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine("Error processing item:");
+                        Console.WriteLine(item.ToString());
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+                var feed = await FormatWotShopFeed(Entries);
+                return Content(feed, "application/xml");
+            }
+            else
+            {
+                Console.WriteLine("Response code is not 0:");
+                Console.WriteLine(RawJson);
+                return StatusCode(503);
+            }
+            
+            
+        }
+
+        private async Task<string> FormatWotShopFeed(IEnumerable<WotShopEntry> entries)
+        {
+            var sw = new StringWriterWithEncoding(Encoding.UTF8);
+
+            using (XmlWriter xmlWriter = XmlWriter.Create(sw, new XmlWriterSettings() { Async = true, Indent = true }))
+            {
+                var writer = new RssFeedWriter(xmlWriter);
+                await writer.WriteTitle("WoTShop360 RSS feed");
+                await writer.WriteDescription("WoTShop360 RSS feed");
+                await writer.Write(new SyndicationLink(new Uri("https://shop.wot.360.cn/")));
+                await writer.WritePubDate(DateTimeOffset.UtcNow);
+
+                foreach (var e in entries)
+                {
+                    try
+                    {
+                        var item = new SyndicationItem()
+                        {
+                            Id = e.WGId,
+                            Title = e.Title,
+                            Published = e.PubDate,
+                            Description = e.Content,
+                        };
+                        item.AddLink(new SyndicationLink(new Uri(e.Link)));
+                        await writer.Write(item);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Issue happens during generating rss for {e.Link}");
+                        Console.WriteLine(e.Title);
+                        Console.WriteLine(e.Content);
+                        Console.WriteLine(e.PubDate);
+
+                        Console.WriteLine(ex.ToString());
+                    }
+                }
+                xmlWriter.Flush();
+            }
+            return sw.ToString();
+        }
+
+        #endregion
     }
 }
